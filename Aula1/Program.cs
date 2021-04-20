da90b9b9744cbbaf7edc2de5b2a4cb6b8a4c9e27
Paulo Henrique <paulo.silva2203@gmail.com>
2021-04-19 22:44:35 -0300

    Adicionar arquivos de projeto.

---
+++ Aula1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Aula1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string nome, sobrenome, resultado;
+
+            Console.Write("Nome: ");
+            nome = Console.ReadLine();
+
+            Console.Write("Sobrenome: ");
+            sobrenome = Console.ReadLine();
+
+            resultado = nome + " " + sobrenome;
+
+            Console.Write("Resultado: " + resultado);
+            Console.ReadLine();
+        }
+    }  
+}
